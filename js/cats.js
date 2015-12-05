04bb53ebf03048a568d046f6322e959982ffab58
sean <sean.cox.or@gmail.com>
2015-12-04 17:45:50 -0800

    inital commit for cats page

---
+++ js/cats.js
@@ -0,0 +1,28 @@
+var cat1 = [
+  'https://pbs.twimg.com/profile_images/3356422133/e5e9271c7b7ac02abc6c6de59c8d85e3.png',
+  'http://www.returnofkings.com/wp-content/uploads/2014/05/fat-cat.jpg',
+  'http://icatcare.org/sites/default/files/kcfinder/images/cat-tap.jpg'
+]
+var cat2 = [
+  'https://pbs.twimg.com/profile_images/567285191169687553/7kg_TF4l.jpeg',
+  'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS1Ud4kJXzBJvBbsoNHyP3ginFdT7in1JAErrmtCe1WyEACDvIvgQ',
+  'http://i3.kym-cdn.com/photos/images/newsfeed/000/012/445/lime-cat.jpg'
+]
+var cat3 = [
+  'https://s-media-cache-ak0.pinimg.com/736x/99/21/41/9921417852ebbf9bc43819faea5ad906.jpg',
+  'http://mitchandgina.com/wp-content/uploads/2014/10/1261839-bigthumbnail.jpg',
+  'http://www.blogyoutrust.com/wp-content/uploads/2014/09/180.jpg'
+]
+var cat4 = [
+  'http://buzzlamp.com/wp-content/uploads/2013/09/cats-stuck-0.jpg',
+  'https://i.ytimg.com/vi/nINuDXRsi0E/maxresdefault.jpg',
+  'http://www.afternoonspecial.com/wp-content/uploads/2015/06/cat-fails-15.jpg'
+]
+var options = {
+  duration: 3000,
+  fade: 100
+}
+$('.1').backstretch(cat1, options)
+$('.2').backstretch(cat2, options)
+$('.3').backstretch(cat3, options)
+$('.4').backstretch(cat4, options)
